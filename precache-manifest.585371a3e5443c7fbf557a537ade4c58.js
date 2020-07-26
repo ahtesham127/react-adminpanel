39bd715921025e909c0dbc320a29921c472d2d6d
ahtesham127 <ahtesham_hussain@outlook.com>
2020-07-26 13:28:36 +0500

    Updates

---
+++ precache-manifest.585371a3e5443c7fbf557a537ade4c58.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01560a4c4cc00adaa943cb9b625d7bfc",
+    "url": "/react-adminpanel/index.html"
+  },
+  {
+    "revision": "22939af9aede12c536ed",
+    "url": "/react-adminpanel/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "93f14095cf88aa5e0e44",
+    "url": "/react-adminpanel/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "22939af9aede12c536ed",
+    "url": "/react-adminpanel/static/js/2.660381d7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-adminpanel/static/js/2.660381d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93f14095cf88aa5e0e44",
+    "url": "/react-adminpanel/static/js/main.876b8d30.chunk.js"
+  },
+  {
+    "revision": "95f6173a6b11445b8c41",
+    "url": "/react-adminpanel/static/js/runtime-main.93815d62.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-adminpanel/static/media/logo.5d5d9eef.svg"
+  }
+]);
